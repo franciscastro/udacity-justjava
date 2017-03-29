61cfcc18d1924bb741f2a7bb5ef6bd8e9780a16b
franciscastro <fcastro.online@gmail.com>
2017-03-29 11:53:59 -0400

    Added functional checkboxes for toppings

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.justjava;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -23,7 +24,10 @@ public class MainActivity extends AppCompatActivity {
     public void submitOrder(View view) {
 
         int price = calculatePrice();
-        String priceMessage = createOrderSummary(price);
+        boolean hasWhippedCream = checkWhippedCream();
+        boolean hasChocolate = checkChocolate();
+
+        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate);
 
         //String priceMessage = "Total: $" + price;
         //priceMessage = priceMessage + "\nThank you!";
@@ -33,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Calculates the price of the order.
+     *
      * @return total price of the order
      */
     private int calculatePrice() {
@@ -40,14 +45,29 @@ public class MainActivity extends AppCompatActivity {
         return price;
     }
 
+    private boolean checkWhippedCream() {
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        return whippedCreamCheckBox.isChecked();
+    }
+
+    private boolean checkChocolate() {
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        return chocolateCheckBox.isChecked();
+    }
+
     /**
      * Generates the order summary
-     * @param price of the order
+     *
+     * @param price           of the order
+     * @param hasWhippedCream is whether the user wants whipped cream or not
+     * @param hasChocolate    is whether the user wants chocolate or not
      * @return a string containing the order summary
      */
-    private String createOrderSummary(int price){
+    private String createOrderSummary(int price, boolean hasWhippedCream, boolean hasChocolate) {
 
         String orderSummary = "Name: Francis";
+        orderSummary += "\nAdd whipped cream? " + hasWhippedCream;
+        orderSummary += "\nAdd chocolate? " + hasChocolate;
         orderSummary += "\nQuantity: " + quantity;
         orderSummary += "\nTotal: $" + price;
         orderSummary += "\nThank you!";
