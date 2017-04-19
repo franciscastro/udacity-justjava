3197d33a2d7252e2529de3a9f308c2e74c5bae6f
franciscastro <fcastro.online@gmail.com>
2017-04-19 11:40:44 -0400

    Added error checks for invalid inputs

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,17 +1,19 @@
 package com.example.android.justjava;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int quantity = 0;
+    private int quantity = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +26,12 @@ public class MainActivity extends AppCompatActivity {
      */
     public void submitOrder(View view) {
 
-        int price = calculatePrice();
         boolean hasWhippedCream = checkWhippedCream();
         boolean hasChocolate = checkChocolate();
         String userName = getUserName();
+
+        // Calculate price
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
 
         String priceMessage = createOrderSummary(userName, price, hasWhippedCream, hasChocolate);
 
@@ -40,14 +44,28 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Calculates the price of the order.
      *
+     * @param hasWhippedCream if the user added whipped cream topping
+     * @param hasChocolate    if the user added chocolate topping
      * @return total price of the order
      */
-    private int calculatePrice() {
-        int price = quantity * 5;
-        return price;
+    private int calculatePrice(boolean hasWhippedCream, boolean hasChocolate) {
+
+        int basePrice = 5;
+
+        // Add $1 if the user adds whipped cream
+        if (hasWhippedCream) {
+            basePrice = basePrice + 1;
+        }
+
+        // Add $2 if the user adds chocolate
+        if (hasChocolate) {
+            basePrice = basePrice + 2;
+        }
+
+        return (basePrice * quantity);
     }
 
-    private String getUserName(){
+    private String getUserName() {
         EditText userNameEditText = (EditText) findViewById(R.id.name_edit_text);
         String userName = userNameEditText.getText().toString();
         return userName;
@@ -90,6 +108,12 @@ public class MainActivity extends AppCompatActivity {
      */
     public void increment(View view) {
 
+        if (quantity == 100) {
+            // Show an error message in a toast
+            Toast.makeText(this, "The maximum order is 100 coffees", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         quantity = quantity + 1;
         displayQuantity(quantity);
 
@@ -100,9 +124,13 @@ public class MainActivity extends AppCompatActivity {
      */
     public void decrement(View view) {
 
-        if (quantity > 0) {
-            quantity = quantity - 1;
+        if (quantity == 1) {
+            // Show an error message in a toast
+            Toast.makeText(this, "The minimum order is 1 coffee", Toast.LENGTH_SHORT).show();
+            return;
         }
+
+        quantity = quantity - 1;
         displayQuantity(quantity);
 
     }
